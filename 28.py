9d7b16fd6e141d0b119e2585cddeb8152664a9ec
Jordan Lewis <jordanthelewis@gmail.com>
2010-06-07 06:02:36 -0500

    Fix 28.py: number spirals

--- 28.py
+++ 28.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python 
 
 
-diagnum = 1001
-halfdiag = 3
-topright = sum([(((2*x) + 1) ** 2) for x in xrange(halfdiag)])
-botleft = sum([((x-1) * (2 ** x)) + 1 for x in xrange(halfdiag)]) - 1
-`:
+size = 1001
+spiral = list(0 for i in range(size * size))
 
+idx = size * (size/2) + size/2
 
-print topright + topleft + botright + botleft
+# 0 = right, 1 = down, 2 = left, 3 = up
+
+diroffsets = [1, size, -1, -size]
+
+num = 1
+dir = 0
+spiral[idx] = num
+num += 1
+idx += 1
+spiral[idx] = num
+num += 1
+dir = 1
+while idx != size - 1:
+    nextdir = (dir + 1) % 4
+    if spiral[idx + diroffsets[nextdir]] == 0:
+        dir = nextdir
+    idx += diroffsets[dir]
+    spiral[idx] = num
+    num += 1
+
+total = -1 # 1 will get counted twice
+for i in range(size):
+    total += spiral[i * size + i]
+    total += spiral[i * size + (size - i - 1)]
+
+print total
+
